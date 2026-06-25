2fba6c7bfde1d9a9fe4c1fd064f9bfea47e90e53
Dave Wilding <tech@dpw.me>
2026-06-25 09:45:24 +0800

    preview latest tutorial charms

---
+++ tests/functional/charms/k8s-1-minimal-patched/src/charm.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+# Copyright 2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Kubernetes charm for a demo app."""
+
+import logging
+
+import ops
+
+import fastapi_demo
+
+# Log messages can be retrieved using juju debug-log
+logger = logging.getLogger(__name__)
+
+
+class FastAPIDemoCharm(ops.CharmBase):
+    """Charm the service."""
+
+    def __init__(self, framework: ops.Framework) -> None:
+        super().__init__(framework)
+        self.pebble_service_name = "fastapi-service"
+        framework.observe(self.on["demo-server"].pebble_ready, self._on_demo_server_pebble_ready)
+
+    def _on_demo_server_pebble_ready(self, event: ops.PebbleReadyEvent) -> None:
+        """Define and start a workload using the Pebble API."""
+        # Get a reference the container attribute on the PebbleReadyEvent
+        container = event.workload
+        # Add initial Pebble config layer using the Pebble API
+        container.add_layer("fastapi_demo", self._get_pebble_layer(), combine=True)
+        # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
+        container.replan()
+        # Learn more about statuses at
+        # https://documentation.ubuntu.com/juju/3.6/reference/status/
+        self.unit.status = ops.ActiveStatus()
+        # Set the workload version of this charm.
+        version = fastapi_demo.get_version(port=8000)
+        self.unit.set_workload_version(version)
+
+    def _get_pebble_layer(self) -> ops.pebble.Layer:
+        """Pebble layer for the FastAPI demo services."""
+        command = " ".join(
+            [
+                "uvicorn",
+                "api_demo_server.app:app",
+                "--host=0.0.0.0",
+                "--port=8000",
+            ]
+        )
+        pebble_layer: ops.pebble.LayerDict = {
+            "summary": "FastAPI demo service",
+            "description": "pebble config layer for FastAPI demo server",
+            "services": {
+                self.pebble_service_name: {
+                    "override": "replace",
+                    "summary": "fastapi demo",
+                    "command": command,
+                    "startup": "enabled",
+                }
+            },
+        }
+        return ops.pebble.Layer(pebble_layer)
+
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(FastAPIDemoCharm)
